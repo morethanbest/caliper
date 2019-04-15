c6fcc19eaa97318e6253a509f034aaccfe9f4e62
wxymac <609402897@qq.com>
2019-04-15 11:55:32 +0800

    test bug

--- src/contract/fisco-bcos/helloworld/HelloWorld.sol
+++ src/contract/fisco-bcos/helloworld/HelloWorld.sol
@@ -1,13 +1,29 @@
 pragma solidity ^0.4.2;
 contract HelloWorld{
     string name;
+    mapping(string=>uint) test;
     function HelloWorld(){
        name="Hi,Welcome!";
     }
     function get()constant returns(string){
-        return name;
+        return uintToBytes(test["123"]);
     }
     function set(string n){
+        test["123"] = 100;
     	name=n;
     }
+}
+
+function uintToBytes(uint v) constant returns (bytes32 ret) {
+    if (v == 0) {
+        ret = '0';
+    }
+    else {
+        while (v > 0) {
+            ret = bytes32(uint(ret) / (2 ** 8));
+            ret |= bytes32(((v % 10) + 48) * 2 ** (8 * 31));
+            v /= 10;
+        }
+    }
+    return ret;
 }
